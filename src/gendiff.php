8eb2db5368dbe85b7a2202a54a71ab9b889993de
Mick Rubashkin <mikhail.rubashkin@gmail.com>
2024-09-09 16:37:55 +0200

    complete steps 1 and 2

---
+++ src/gendiff.php
@@ -0,0 +1,21 @@
+<?php
+
+$doc = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help       Show this screen
+  -v --version    Show version
+  --format <fmt>  Report format [default: stylish]
+
+DOC;
+
+$args = Docopt::handle($doc);
+
+
+echo 'hey hey!';
