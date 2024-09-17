756449d91297b1a2afe1c7c2926be0b8f625c13e
Mick Rubashkin <mikhail.rubashkin@gmail.com>
2024-09-17 14:46:11 +0200

    refactor calcDiff (functional style)

--- src/CalculateDiff.php
+++ src/CalculateDiff.php
@@ -7,45 +7,21 @@ use function Differ\Functions\isAssociativeArray;
 function calcDiff(array $coll1, array $coll2): array
 {
     $mergedColl = array_merge($coll1, $coll2);
-    $diff = [];
 
-    foreach ($mergedColl as $k => $v) {
+    $diff = array_map(function ($k, $v) use ($coll1, $coll2) {
         if (!array_key_exists($k, $coll1)) {
-            $diff[] = [
-                'key' => $k,
-                'type' => 'added',
-                'value' => $v,
-            ];
+            return ['key' => $k, 'type' => 'added', 'value' => $v];
         } elseif (!array_key_exists($k, $coll2)) {
-            $diff[] = [
-                'key' => $k,
-                'type' => 'removed',
-                'value' => $v,
-            ];
+            return ['key' => $k, 'type' => 'removed', 'value' => $v];
         } elseif (isAssociativeArray($v)) {
-            $inner = calcDiff($coll1[$k], $coll2[$k]);
-            $diff[] = [
-                'key' => $k,
-                'type' => 'nested',
-                'children' => $inner,
-            ];
+            $children = calcDiff($coll1[$k], $coll2[$k]);
+            return ['key' => $k, 'type' => 'nested', 'children' => $children];
         } elseif ($coll1[$k] === $coll2[$k]) {
-            $diff[] = [
-                'key' => $k,
-                'type' => 'unchanged',
-                'value' => $v,
-            ];
+            return ['key' => $k, 'type' => 'unchanged', 'value' => $v];
         } else {
-            $diff[$k] = [
-                'key' => $k,
-                'type' => 'updated',
-                'value' => [
-                    'from' => $coll1[$k],
-                    'to' => $coll2[$k]
-                ]
-            ];
+            return ['key' => $k, 'type' => 'updated', 'value' => ['from' => $coll1[$k], 'to' => $coll2[$k]]];
         }
-    }
+    }, array_keys($mergedColl), array_values($mergedColl));
 
     usort($diff, fn ($v1, $v2) => $v1['key'] <=> $v2['key']);
 
