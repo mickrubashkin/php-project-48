c3a72c25005d837224e5dab95536207d502d7fbd
Mick Rubashkin <mikhail.rubashkin@gmail.com>
2024-09-17 10:14:26 +0200

    fix hexlet-check warnings

--- src/Differ.php
+++ src/Differ.php
@@ -11,13 +11,6 @@ function genDiff(string $path1, string $path2, string $format = 'stylish'): stri
     $realPath1 = realpath($path1);
     $realPath2 = realpath($path2);
 
-    $file1Exists = $realPath1 !== false;
-    $file2Exists = $realPath2 !== false;
-
-    if (!$file1Exists || !$file2Exists) {
-        echo 'File does not exists. Please check the pathes.' . PHP_EOL;
-    }
-
     $coll1 = parse($realPath1);
     $coll2 = parse($realPath2);
 
--- src/Parsers.php
+++ src/Parsers.php
@@ -4,16 +4,25 @@ namespace Differ\Parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
-function parse(string $path)
+function parse(string|false $path)
 {
-    $content = file_get_contents($path);
-    $fileType = pathinfo($path)['extension'];
-
-    if ($fileType === 'json') {
-        return json_decode($content, true);
+    if ($path === false) {
+        echo 'File does not exists. Please check the pathes.' . PHP_EOL;
     }
 
-    if ($fileType === 'yaml' || $fileType === 'yml') {
-        return Yaml::parse($content);
+    $content = file_get_contents($path);
+    $fileType = pathinfo($path, PATHINFO_EXTENSION);
+
+    if ($content === false) {
+        echo 'Failed to read file.' . PHP_EOL;
+    }
+    if (gettype($content) === 'string') {
+        if ($fileType === 'json') {
+                return json_decode($content, true);
+        }
+
+        if ($fileType === 'yaml' || $fileType === 'yml') {
+            return Yaml::parse($content);
+        }
     }
 }
