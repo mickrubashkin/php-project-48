0f39641ff73ef315689bd9aef8051e6a5cdf656d
Mick Rubashkin <mikhail.rubashkin@gmail.com>
2024-09-17 15:37:43 +0200

    refactor to immutable Json formatter

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -10,16 +10,14 @@ function convertToAssoc(array $arr)
         if ($type === 'nested') {
             $children = $item['children'];
             $newChildren = convertToAssoc($children);
-            $acc[$key] = ['type' => $type, 'children' => $newChildren];
+            return [...$acc, $key => ['type' => $type, 'children' => $newChildren]];
         } elseif ($type === 'updated') {
             ['from' => $from, 'to' => $to] = $item['value'];
-            $acc[$key] = ['type' => $type, 'from' => $from, 'to' => $to];
+            return [...$acc, $key => ['type' => $type, 'from' => $from, 'to' => $to]];
         } else {
             $value = $item['value'];
-            $acc[$key] = ['type' => $type, 'value' => $value];
+            return [...$acc, $key => ['type' => $type, 'value' => $value]];
         }
-
-        return $acc;
     }, []);
 
     return $coll;
